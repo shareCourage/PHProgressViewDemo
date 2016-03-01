13469cdf57737c4b09027acaf252753190c9fb53
PaulMac <panhaohua2014@yahoo.com>
2016-03-01 16:39:22 +0800

    删除没用搞定代码

--- PHProgressView/PHProgressView/ViewController.m
+++ PHProgressView/PHProgressView/ViewController.m
@@ -16,13 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"git test");
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
